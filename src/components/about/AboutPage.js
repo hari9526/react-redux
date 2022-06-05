665637bae6d3feb6e07d8bdabb14d7b073c790fe
Harikrishnan R <hari04.krishnan@gmail.com>
2022-06-06 01:45:07 +0530

    Create the following pages
    -Page not found
    -Home
    -Courses
    -About
    -Header

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+
+
+const AboutPage = () => (
+    <div>
+        <h2>About</h2>
+        <p>
+            This app uses React, Redux, and many other libraries
+        </p>
+
+    </div>
+); 
+
+
+export default AboutPage; 
